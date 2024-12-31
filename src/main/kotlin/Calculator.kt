5e3bc0f746ecd98701440460c36f9dee4c968edf
eshanjain <eshanjain1000@gmail.com>
2024-12-31 11:59:22 -0800

    Create Calculator.kt

---
+++ src/main/kotlin/Calculator.kt
@@ -0,0 +1,6 @@
+class Calculator {
+    fun add(a: Int, b: Int): Int = a + b
+    fun subtract(a: Int, b: Int): Int = a - b
+    fun multiply(a: Int, b: Int): Int = a * b
+    fun divide(a: Int, b: Int): Double = a.toDouble() / b.toDouble()
+}
